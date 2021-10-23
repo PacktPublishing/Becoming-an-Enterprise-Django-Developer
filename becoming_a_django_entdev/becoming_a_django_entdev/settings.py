c042fbbecf5bf14a93ea2ce5a771f3dfef4f8ea8
elite-dinder <michael.dinder@elitepacific.com>
2021-10-22 21:59:14 -0700

    Core file structure changes made to satisfy concerns raised from Chapter 2 - Technical Feedback.

--- becoming_a_django_entdev/becoming_a_django_entdev/settings.py
+++ becoming_a_django_entdev/becoming_a_django_entdev/settings.py
@@ -31,7 +31,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 INTERNAL_IPS = [
     '127.0.0.1',
